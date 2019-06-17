10483d6ca7e5947f07e9c94c53a40c252db1f5b2
Jolly Rogerz <voyagercoinz@gmail.com>
2019-06-17 13:23:25 +0100

    1st

---
+++ sw.js
@@ -0,0 +1,77 @@
+const VERSION = '1'
+
+const ASSETS = [
+    '/',
+    'index.html',
+    'src/script.js',
+    'src/snake.js',
+    'sw.js',
+    'src/icons/icon@96.png',
+    'src/icons/icon@128.png',
+    'src/icons/icon@256.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.5/p5.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.2/lodash.js',
+    'manifest.json'
+]
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('static')
+        .then(cache => {
+            return cache.addAll(ASSETS)
+                .then(_ => self.skipWaiting())
+        })
+    )
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim())
+});
+
+self.addEventListener('fetch', event => {
+
+    var response;
+    event.respondWith(
+        caches.match(event.request)
+        .catch(_ => fetch(event.request))
+        .then(function(r) {
+            response = r;
+            caches.open('dinamic')
+                .then(function(cache) {
+                    cache.put(event.request, response)
+                });
+            return response.clone()
+        }).catch(function(event) {
+            console.log(event, 'broken')
+            return caches.match(event.request)
+        })
+    )
+})
+
+
+self.oninstall = event => event.waitUntil(async function() {
+    const cache = await caches.open('static')
+
+    await cache.addAll(ASSETS)
+    return self.skipWaiting()
+}())
+
+self.onactivate = event => event.waitUntil(self.clients.claim())
+
+function staleWhileRevalidateStrategy() {
+    let fetchedVersion = fetch(event.request),
+        cachedVersion = caches.match(event.request)
+
+    event.respondWith(async function() {
+        const response = Promise.race([
+            fetchedVersion.catch(_ => cachedVersion),
+            cachedVersion
+        ])
+
+        if (!response) {
+            return await fetchedVersion
+        }
+
+        return response
+    })
+}
